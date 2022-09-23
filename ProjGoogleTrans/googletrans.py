8d27e0ef8462200397df59b55af8dc7a0c269ae4
amanda <kunbu@mail.usf.edu>
2022-09-22 20:28:06 -0400

    googletrans

--- ProjGoogleTrans/googletrans.py
+++ ProjGoogleTrans/googletrans.py
@@ -5,9 +5,9 @@
 from googletrans import Translator
 translator = Translator()
 
-translate_channel = translator.translate('안녕하세요', dest='ja')
+translate_channel = translator.translate('जारी रखना', dest='ja')
 
 #translate_channel = translator.translate('Canal La Tele Perú', src='es', dest='en').group()
 #translate_channel2 = translator.translate('La defensa y las acciones ofensivas de Alex Dujshebaev dan a Españasu cuarto bronce en unos Juegos tras los de Atlanta 1996, Sydney 2000 y Pekín 2008.', src='es', dest='en')
 #print(translate_channel)
-#print(translate_channel2)
+#print(translate_channel2) 
