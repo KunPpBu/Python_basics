1af34ff573a4a419b7cd1153dd1d58e4add77ec6
Kun Bu <kunbu@mail.usf.edu>
2022-12-09 17:47:56 -0500

    Exercise SGD

---
+++ Hecker_OpenClass/机器学习训练营第五期/Lesson 04.逻辑回归基本原理与手动实现方法/Exercise_Stochastic_gradient_descent.py
@@ -0,0 +1,14 @@
+# Sicience modules
+import numpy as np
+import pandas as pd
+
+# polotting module
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Self define functions
+from ML_basic_function import *
+
+
+w = 0
