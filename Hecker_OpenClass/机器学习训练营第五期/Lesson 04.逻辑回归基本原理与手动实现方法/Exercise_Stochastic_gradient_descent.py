b4ee16105e445e1d118cbd498b7ad6c0514bf820
Kun Bu <kunbu@mail.usf.edu>
2022-12-15 20:01:42 -0500

    updated SGD 12152022

--- Hecker_OpenClass/机器学习训练营第五期/Lesson 04.逻辑回归基本原理与手动实现方法/Exercise_Stochastic_gradient_descent.py
+++ Hecker_OpenClass/机器学习训练营第五期/Lesson 04.逻辑回归基本原理与手动实现方法/Exercise_Stochastic_gradient_descent.py
@@ -22,3 +22,30 @@ w = w_cal(x[1], w, y[1], lr_gd, lr = 0.02, itera_times = 1)
 w
 w = 0
 w_cal(x, w, y, lr_gd, lr = 0.02, itera_times = 2)
+
+w = 0
+epoch = 40
+
+for j in range(epoch):
+    for i in range(2):
+        w = w_cal(x[i], w, y[i], lr_gd, lr = 0.02, itera_times = 1)
+
+w
+
+def sgd_cal(X, w, y, gd_cal, epoch, lr = 0.02):
+    """
+    随机梯度下降计算函数
+    :param X: 训练数据特征
+    :param w: 初始参数取值
+    :param y: 训练数据标签
+    :param gd_cal：梯度计算公式
+    :param epoch: 遍历数据集次数
+    :param lr: 学习率      
+    :return w：最终参数计算结果       
+    """
+    m = X.shape[0]
+    n = X.shape[1]
+    for j in range(epoch):
+        for i in range(m):
+            w = w_cal(X[i].reshape(1, n), w, y[i].reshape(1, 1), gd_cal=gd_cal, lr=lr, itera_times = 1)
+    return w
